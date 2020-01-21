d7eb3bf404de5e255beb4b14ab1ead0d3d67f181
SnarkDev <elia.scherrer@gmx.ch>
2020-01-21 10:44:48 +0100

    extendet the beer class added function .toString() added attributes

--- src/Beer.java
+++ src/Beer.java
@@ -1,2 +1,18 @@
 public class Beer {
+
+    String id;
+    String name;
+    String description;
+    int idStyle;
+
+
+    public String toString(String id, String name, String description, int idStyle)
+    {
+        String resultString = "";
+
+        Integer.toString(idStyle);
+        resultString = id + " ; " + name + " ; " + description + " ; " + idStyle;
+
+        return resultString;
+    }
 }
